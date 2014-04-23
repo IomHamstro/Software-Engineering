db15218abbb71ec650671bbbd53654f5851260c7
Ольга <telejnikova.olga@gmail.com>
2014-04-23 23:49:25 +0400

    changeDone

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-        for(int i=0; i < 20; i++)
+        for(int i=0; i < 13; i++)
             System.out.println("I’m watching you");
     }
 }
